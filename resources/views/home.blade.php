c1cb65731dfab90c68848462f8b7e86ffe63c96d
Dennis Wanjiru <denniswanjiru71@gmail.com>
2017-05-12 15:05:28 +0300

    Added a conditional Auth check determine the division of the page you should see according to your auth status

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,25 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            @if(Auth::check())
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+
+                    <div class="panel-body">
+                        You are logged in!
+                    </div>
+                </div>
+            @else
+
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    Create an Account or Login!
                 </div>
             </div>
+
+            @endif
         </div>
     </div>
 </div>
